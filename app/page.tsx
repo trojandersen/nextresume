3a9eb48fa9d5f45fcd98a4e3ba575e4dfcadc473
Troj Andersen <trojandersen@gmail.com>
2024-07-31 14:33:52 +0200

    small fixes

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,10 @@ export default function Page() {
       <p className="mb-4">
         {`My name is Troj Andersen, I'm a developer currently studying a 2-year programme at Folkuniversitet in Gothenburg.
         Im mainly focused in improving my skills using the MERN-stack before exploring different techstacks. Some of the tools I've picked up so far include
-        HTML, CSS, Figma, JS/TS, Scrum, React, Node, MongoDB, Next.js & TailwindCSS. Previously I studied as a backend dev where I learnt Java, testing, git, mySQL, 
-        REST, Insomnia. I also learnt more theoretical knowledge in OOP about abstraction, encapsulation, inheritance, the stack, TDD and CI/CD amongst other concepts.`}
+        HTML, CSS, Figma, JS/TS, Scrum, React, Node, MongoDB, Next.js & TailwindCSS.`}
       </p>
+      <p className="mb-4">{`Previously I studied as a backend dev where I learnt Java, testing, git, mySQL, 
+        REST, Insomnia. I also learnt more theoretical knowledge in OOP about abstraction, encapsulation, inheritance, the stack, TDD and CI/CD amongst other concepts.`}</p>
       <div className="my-8">
         <BlogPosts />
       </div>
